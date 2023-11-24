23c8889eeb2d6a94d20586c3090012ad8c9d6fec
PiyushPal <piyushpal09255@gmail.com>
2023-11-24 15:57:06 +0530

    self resolving error

--- Server.js
+++ Server.js
@@ -24,16 +24,16 @@ app.use(express.urlencoded({extended:false}));
 //   optionsSuccessStatus: 204,
 // };
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://flimfair-frontend.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, UPDATE, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://flimfair-frontend.vercel.app');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, UPDATE, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// });
 
-app.use(cors({
-    origin: "https://flimfair-frontend.vercel.app",
-}));
+// app.use(cors({
+//     origin: "https://flimfair-frontend.vercel.app",
+// }));
 
 app.get("/", (req, res)=>{
     res.send("FlimFair Website Server")
