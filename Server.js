909bfc8982778a9a7364be8f3db52f69c044eeec
PiyushPal <piyushpal09255@gmail.com>
2023-11-25 13:40:56 +0530

    added allow origin line

--- Server.js
+++ Server.js
@@ -19,6 +19,12 @@ app.use(express.urlencoded({extended:false}));
 
 app.use(cors())
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
+
 // const corsOptions = {
 //   origin: ["https://flimfair-frontend.vercel.app", "http://localhost:3000"],
 //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
